54f59698da4e90fd907d7a1dc3cbcbb907dd4be8
Zac-HD <zac.hatfield.dodds@gmail.com>
2020-01-06 13:54:14 +1100

    test quodpri round-trip

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import ast
 import imghdr
 import io
+import quopri
 import tokenize
 import unittest
 
@@ -93,3 +94,11 @@ class TestTokenize(unittest.TestCase):
         )
         # It would be nice if the round-tripped string stabilised...
         # self.assertEqual(outstring, tokenize.untokenize(output))
+
+
+class TestQuodpri(unittest.TestCase):
+    @given(payload=st.binary(), quotetabs=st.booleans(), header=st.booleans())
+    def test_quodpri_encode_decode_round_trip(self, payload, quotetabs, header):
+        encoded = quopri.encodestring(payload, quotetabs=quotetabs, header=header)
+        decoded = quopri.decodestring(encoded, header=header)
+        self.assertEqual(payload, decoded)
