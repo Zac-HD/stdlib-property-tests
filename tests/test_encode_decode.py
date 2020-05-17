d56230fa0fd5cfa4d4f4f0ddde994c8606b52246
akashsuresh96 <akash.s1@myntra.com>
2020-05-17 22:34:38 +0530

    - Added arguments altchars, validate to b64encode & b64decode - Added arguments casefold, map01 to b32decode - Added arguments casefold to b16encode & b16decode - Updated wrapcol argument strategy of a85encode to take 0 more often. - Added argument pad to a85encode & a85decode - Added argument pad to b85encode & b85decode - Fixed usage of pad argument in a85decode & b85decode, by manually adding padding when len(payload)!=4

--- tests/test_encode_decode.py
+++ tests/test_encode_decode.py
@@ -5,11 +5,29 @@ import unittest
 from hypothesis import given, strategies as st
 
 
+# function which adds padding, till len(payload) is a multiple of 4
+def add_padding(payload):
+    padding = b"\0" * ((-len(payload)) % 4)
+    payload = payload + padding
+    return payload
+
+
 class TestBase64(unittest.TestCase):
-    @given(payload=st.binary())
-    def test_b64_encode_decode_round_trip(self, payload):
-        x = base64.b64encode(payload)
-        self.assertEqual(payload, base64.b64decode(x))
+    # @given(
+    #     payload=st.binary(),
+    #     altchars=(st.none() | st.binary(min_size=2, max_size=2)),
+    #     validate=st.booleans(),
+    # )
+    @given(
+        payload=st.binary(),
+        altchars=(st.none() | st.just(b"_-")),
+        validate=st.booleans(),
+    )
+    def test_b64_encode_decode_round_trip(self, payload, altchars, validate):
+        x = base64.b64encode(payload, altchars=altchars)
+        self.assertEqual(
+            payload, base64.b64decode(x, altchars=altchars, validate=validate)
+        )
 
     @given(payload=st.binary())
     def test_standard_b64_encode_decode_round_trip(self, payload):
@@ -21,41 +39,46 @@ class TestBase64(unittest.TestCase):
         x = base64.urlsafe_b64encode(payload)
         self.assertEqual(payload, base64.urlsafe_b64decode(x))
 
-    @given(payload=st.binary())
-    def test_b32_encode_decode_round_trip(self, payload):
+    @given(
+        payload=st.binary(),
+        casefold=st.booleans(),
+        map01=(st.none() | st.binary(min_size=1, max_size=1)),
+    )
+    def test_b32_encode_decode_round_trip(self, payload, casefold, map01):
         x = base64.b32encode(payload)
-        self.assertEqual(payload, base64.b32decode(x))
+        self.assertEqual(payload, base64.b32decode(x, casefold=casefold, map01=map01))
 
-    @given(payload=st.binary())
-    def test_b16_encode_decode_round_trip(self, payload):
+    @given(payload=st.binary(), casefold=st.booleans())
+    def test_b16_encode_decode_round_trip(self, payload, casefold):
         x = base64.b16encode(payload)
-        self.assertEqual(payload, base64.b16decode(x))
+        self.assertEqual(payload, base64.b16decode(x, casefold=casefold))
 
     @given(
         payload=st.binary(),
         foldspaces=st.booleans(),
-        wrapcol=st.integers(0, 10),
+        wrapcol=(st.just(0) | st.integers(0, 1000)),
+        pad=st.booleans(),
         adobe=st.booleans(),
     )
-    def test_a85_encode_decode_round_trip(self, payload, foldspaces, wrapcol, adobe):
+    def test_a85_encode_decode_round_trip(
+        self, payload, foldspaces, wrapcol, pad, adobe
+    ):
         x = base64.a85encode(
-            payload, foldspaces=foldspaces, wrapcol=wrapcol, adobe=adobe
+            payload, foldspaces=foldspaces, wrapcol=wrapcol, pad=pad, adobe=adobe
         )
+        # adding padding manually to payload, when pad is True and len(payload)%4!=0
+        if pad:
+            payload = add_padding(payload)
         self.assertEqual(
             payload, base64.a85decode(x, foldspaces=foldspaces, adobe=adobe)
         )
 
-    @given(payload=st.binary())
-    def test_b85_encode_decode_round_trip(self, payload):
-        x = base64.b85encode(payload)
-        self.assertEqual(payload, base64.b85decode(x))
-
-    # this is failing for payload = b'\x00'
-    # expected to remove padding implicitly
-    @unittest.expectedFailure
-    @given(payload=st.binary())
-    def test_b85_encode_with_padding_decode_round_trip(self, payload):
-        x = base64.b85encode(payload, True)
+    @given(payload=st.binary(), pad=st.booleans())
+    def test_b85_encode_decode_round_trip(self, payload, pad):
+        x = base64.b85encode(payload, pad=pad)
+        # adding padding manually to payload, when pad is True and len(payload)%4!=0
+        if pad:
+            payload = add_padding(payload)
         self.assertEqual(payload, base64.b85decode(x))
 
 
